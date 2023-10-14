11c4213740b24c0d7f2a421fb530a1365112c191
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-10-14 20:08:18 +0900

    WIP: 사이트 구성 중

---
+++ myapp/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* 홈페이지 */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'MindMesh', pageName: 'home/home.ejs'});
+});
+
+module.exports = router;
